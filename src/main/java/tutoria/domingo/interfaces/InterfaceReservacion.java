cc49644be055c913f0fe70cd513ebf54001f9849
OneCode182 <86318393+OneCode182@users.noreply.github.com>
2021-10-30 00:27:56 -0500

    Add files via upload

---
+++ src/main/java/tutoria/domingo/interfaces/InterfaceReservacion.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package tutoria.domingo.interfaces;
+
+import java.util.Date;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import tutoria.domingo.modelo.Reservacion;
+
+/**
+ *
+ * @author USUARIO
+ */
+public interface InterfaceReservacion extends CrudRepository<Reservacion,Integer> {
+    
+    public List<Reservacion> findAllByStatus (String status);
+    
+    public List<Reservacion> findAllByStartDateAfterAndStartDateBefore(Date dateOne, Date dateTwo);
+
+    //select clientid, count(*) as "total" from reservacion group by clientid order by total desc; 
+    @Query("SELECT c.client, COUNT(c.client) FROM Reservacion AS c group by c.client order by COUNT(c.client)DESC")
+    public List<Object[]> countTotalReservationByClient();
+    
+}
